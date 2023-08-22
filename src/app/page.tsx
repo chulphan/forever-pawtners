6fcf4bfade5a6cc64a9f1ee41720ec050069620e
chulphan <chkim100617@gmail.com>
2023-08-22 16:34:10 +0900

    vercel 빌드 성공을 위해서 api 호출부제거..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,30 +13,30 @@ import SearchBox from './_components/SearchBox';
 import { getCities, getFullCities, getPaws } from './_lib/api';
 
 export default async function Home() {
-  const cities = (await getCities()) ?? [];
-  const orgCd = cities?.[0]?.orgCd;
-  const fullCities = (await getFullCities(orgCd)) ?? [];
-  const pawsResponseBody = await getPaws({
-    pageNo: 1,
-    numOfRows: 48,
-    totalCount: 0,
-  });
+  // const cities = (await getCities()) ?? [];
+  // const orgCd = cities?.[0]?.orgCd;
+  // const fullCities = (await getFullCities(orgCd)) ?? [];
+  // const pawsResponseBody = await getPaws({
+  //   pageNo: 1,
+  //   numOfRows: 48,
+  //   totalCount: 0,
+  // });
 
-  const paws = pawsResponseBody.items.item;
-  const numOfRows = pawsResponseBody.numOfRows ?? 0;
-  const pageNo = pawsResponseBody.pageNo ?? 0;
-  const totalCount = pawsResponseBody.totalCount ?? 0;
+  // const paws = pawsResponseBody.items.item;
+  // const numOfRows = pawsResponseBody.numOfRows ?? 0;
+  // const pageNo = pawsResponseBody.pageNo ?? 0;
+  // const totalCount = pawsResponseBody.totalCount ?? 0;
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <main className='flex min-h-screen flex-col items-center justify-between p-6 gap-4'>
-        <SearchBox citiesParam={cities} fullCitiesParam={fullCities} />
+        {/* <SearchBox citiesParam={cities} fullCitiesParam={fullCities} />
         <Paws
           pawsParam={paws}
           numOfRowsParam={numOfRows}
           pageNoParam={pageNo}
           totalCountParam={totalCount}
-        />
+        /> */}
       </main>
     </Suspense>
   );
