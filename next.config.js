6561180a23fae5207f24822963e27f04cac19470
chulphan <chkim100617@gmail.com>
2025-02-04 19:40:43 +0900

    deprecate 된 image optimize 설정 최신화

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['www.animal.go.kr'],
+    // domains: ['www.animal.go.kr'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'www.animal.go.kr',
+        port: '',
+      },
+    ],
   },
 };
 
