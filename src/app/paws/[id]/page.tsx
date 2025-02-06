761047d9b78e55c7b686ac62fbb89c2e2d0d9125
chulphan <chkim100617@gmail.com>
2025-02-06 20:48:16 +0900

    null 가능성 제거

--- src/app/paws/[id]/page.tsx
+++ src/app/paws/[id]/page.tsx
@@ -1,5 +1,6 @@
 import MapDialog from '@/app/_components/MapDialog';
 import { createClient } from '@/lib/supabase/server';
+import { notFound } from 'next/navigation';
 
 export default async function PawsPage({
   params,
@@ -12,7 +13,15 @@ export default async function PawsPage({
   console.log('id', id);
   const supabase = await createClient();
   const paw = await supabase.from('paw').select(`*`).eq('desertionNo', id);
-  console.log('paw detail ', paw);
+
+  if (!paw) {
+    notFound();
+  }
+
+  if (!paw.data) {
+    notFound();
+  }
+
   return (
     <div>
       <MapDialog address={paw.data[0].careAddr} />
