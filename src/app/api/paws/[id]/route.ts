c465e8844b83f2eed3e73a889fd25ba8b456e9b2
chulphan <chkim100617@gmail.com>
2025-02-18 16:54:09 +0900

    유기동물 상세 조회를 위한 app router 생성

--- src/app/api/paws/[id]/route.ts
+++ src/app/api/paws/[id]/route.ts
@@ -1,8 +1,11 @@
 import { createClient } from '@/lib/supabase/server';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
-  const id = request.nextUrl.searchParams.get('id');
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const id = (await params).id;
   const supabase = await createClient();
 
   const { data, error } = await supabase
