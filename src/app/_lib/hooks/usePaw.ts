c5d273890cbc010f8ac6d77c75acf681b021bf5b
chulphan <chkim100617@gmail.com>
2025-02-18 11:51:09 +0900

    상세정보 가져오는 함수 + hook 생성

--- src/app/_lib/hooks/usePaw.ts
+++ src/app/_lib/hooks/usePaw.ts
@@ -1,5 +1,5 @@
 import { Paw } from '@/app/_types';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 const useWritePaw = () => {
   return useMutation({
@@ -11,4 +11,30 @@ const useWritePaw = () => {
   });
 };
 
-export { useWritePaw };
+const fetchPawById = async (id: string) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SITE_URL}/api/paws/${id}`
+  );
+
+  if (!response.ok) {
+    return null;
+  }
+
+  const result = await response.json();
+
+  if (result.error) {
+    console.error('error ', JSON.stringify(result));
+    return null;
+  }
+
+  return result.data;
+};
+
+const useFetchPawById = (id: string) => {
+  return useQuery({
+    queryKey: ['paw', id],
+    queryFn: () => fetchPawById(id),
+  });
+};
+
+export { useWritePaw, fetchPawById, useFetchPawById };
