2db10a0cb10d5166f66382dab90ed067de88cb82
chulphan <chkim100617@gmail.com>
2025-02-18 11:13:34 +0900

    유기동물 정보 상세 가져오는 로직 임시 제거

--- src/app/_lib/hooks/usePaw.ts
+++ src/app/_lib/hooks/usePaw.ts
@@ -1,6 +1,5 @@
 import { Paw } from '@/app/_types';
-import { createClient } from '@/lib/supabase/server';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 const useWritePaw = () => {
   return useMutation({
@@ -12,23 +11,4 @@ const useWritePaw = () => {
   });
 };
 
-const fetchPawById = async (id: string): Promise<Paw | null> => {
-  const supabase = await createClient();
-  const result = await supabase.from('paw').select(`*`).eq('desertionNo', id);
-
-  if (!result?.data) {
-    return null;
-  }
-
-  return result.data[0] as Paw;
-};
-
-const useFetchPawById = (id: string) => {
-  return useQuery({
-    queryKey: ['paw', id],
-    queryFn: () => fetchPawById(id),
-    enabled: !!id,
-  });
-};
-
-export { useWritePaw, useFetchPawById };
+export { useWritePaw };
