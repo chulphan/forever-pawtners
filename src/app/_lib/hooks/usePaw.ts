d128c98965f50a6b8670711c18ad0de2a99b17a9
chulphan <chkim100617@gmail.com>
2025-02-20 14:25:33 +0900

    유기동물 DB 등록 요청 시 update 된 경우 react query data 다시 설정하도록 변경

--- src/app/_lib/hooks/usePaw.ts
+++ src/app/_lib/hooks/usePaw.ts
@@ -1,13 +1,24 @@
 import { Paw } from '@/app/_types';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { QueryClient, useMutation, useQuery } from '@tanstack/react-query';
 
 const useWritePaw = () => {
+  const queryClient = new QueryClient();
   return useMutation({
     mutationFn: ({ paw }: { paw: Paw }) =>
       fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/paws`, {
         method: 'POST',
         body: JSON.stringify(paw),
       }),
+    onSuccess: async (response, variable) => {
+      const result = await response.json();
+
+      if (result.message === 'updated') {
+        queryClient.setQueryData(
+          ['paw', variable.paw.desertionNo],
+          variable.paw
+        );
+      }
+    },
   });
 };
 
