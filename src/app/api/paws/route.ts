94722a72ff6e59397c1ae3f8e15495ac16bff0ff
chulphan <chkim100617@gmail.com>
2025-02-20 14:23:33 +0900

    유기동물 조회 시 이미 DB에 존재하는 경우 update 처리

--- src/app/api/paws/route.ts
+++ src/app/api/paws/route.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@/lib/supabase/server';
 import { NextRequest, NextResponse } from 'next/server';
 
+const DUPLICATE_ERROR_CODE = '23505';
+
 export async function POST(request: NextRequest) {
   const paw = await request.json();
   const supabase = await createClient();
@@ -8,8 +10,22 @@ export async function POST(request: NextRequest) {
   const { error } = await supabase.from('paw').insert([paw]);
 
   if (error) {
+    if (error.code === DUPLICATE_ERROR_CODE) {
+      await supabase.from('paw').update([paw]);
+      return NextResponse.json({
+        result: 'ok',
+        message: 'upserted',
+        data: paw,
+      });
+    }
+
     console.log('error ', JSON.stringify(error));
-    return NextResponse.json({ result: 'error', error });
+    return NextResponse.json(
+      { result: 'error', error },
+      {
+        status: 500,
+      }
+    );
   }
 
   return NextResponse.json({ result: 'ok' });
