b363dc31573fe10b5d755424327ac59255e0f003
chulphan <chkim100617@gmail.com>
2025-02-20 13:45:56 +0900

    server side 에 맞도록 Provider 코드 수정

--- src/app/_lib/providers/ReactQueryProvider.tsx
+++ src/app/_lib/providers/ReactQueryProvider.tsx
@@ -1,17 +1,44 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { ReactNode } from 'react';
 
-const queryClient = new QueryClient();
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) {
+      browserQueryClient = makeQueryClient();
+    }
+    return browserQueryClient;
+  }
+}
 
 export default function ReactQueryProvider({
   children,
 }: {
   children: ReactNode;
 }) {
+  const queryClient = getQueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
