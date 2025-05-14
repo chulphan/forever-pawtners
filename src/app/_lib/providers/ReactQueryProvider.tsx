ceefb7b50a8c4fa83bc0c7d0d9e63c7ecfb06e80
chulphan <chkim100617@gmail.com>
2025-05-14 17:43:14 +0900

    query client 분리

--- src/app/_lib/providers/ReactQueryProvider.tsx
+++ src/app/_lib/providers/ReactQueryProvider.tsx
@@ -1,41 +1,10 @@
 'use client';
 
-import {
-  defaultShouldDehydrateQuery,
-  isServer,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { ReactNode } from 'react';
-
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 1000 * 60,
-      },
-      dehydrate: {
-        shouldDehydrateQuery: (query) =>
-          defaultShouldDehydrateQuery(query) || query.state.status === 'pending',
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (isServer) {
-    return makeQueryClient();
-  } else {
-    if (!browserQueryClient) {
-      browserQueryClient = makeQueryClient();
-    }
-    return browserQueryClient;
-  }
-}
+import { getQueryClient } from '../util/get-query-client';
 
 export default function ReactQueryProvider({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
