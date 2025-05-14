ad4c40865d1bb16b02921a5131e981f5882f9f5c
chulphan <chkim100617@gmail.com>
2025-05-15 07:26:07 +0900

    시도군구 useQuery 훅 생성

---
+++ src/app/_lib/hooks/react-query/useSigungu.ts
@@ -0,0 +1,10 @@
+import { useQuery } from '@tanstack/react-query';
+import { getFullCities } from '../../api';
+
+export const useSigungu = (uprCd?: string) => {
+  return useQuery({
+    queryKey: ['sigugu', uprCd],
+    queryFn: () => getFullCities(uprCd),
+    enabled: !!uprCd,
+  });
+};
