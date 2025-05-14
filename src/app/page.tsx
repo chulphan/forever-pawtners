8cd55b8b822b1a4eb522ce533ca79dc260c17d85
chulphan <chkim100617@gmail.com>
2025-05-14 17:44:06 +0900

    유기동물 목록 조회 prefetch 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,23 @@ import { Suspense } from 'react';
 import Paws from './_components/Paws';
 import SearchBox from './_components/SearchBox';
 import { getCities, getPaws } from './_lib/api';
+import { pawsQueryOptions } from './_lib/hooks/react-query/usePaws';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { getQueryClient } from './_lib/util/get-query-client';
 
 export default async function Home() {
+  const queryClient = getQueryClient();
   const cities = (await getCities()) ?? [];
-  const pawsResponseBody = await getPaws({
-    pageNo: 1,
-    numOfRows: 48,
-    totalCount: 0,
-  });
 
-  const paws = pawsResponseBody.items.item;
-  const numOfRows = pawsResponseBody.numOfRows ?? 0;
-  const pageNo = pawsResponseBody.pageNo ?? 0;
-  const totalCount = pawsResponseBody.totalCount ?? 0;
+  void queryClient.prefetchInfiniteQuery(pawsQueryOptions());
 
   return (
     <Suspense>
-      <main className='flex min-h-screen flex-col items-center p-6 gap-4'>
+      <main className="flex min-h-screen flex-col items-center p-6 gap-4">
         <SearchBox citiesParam={cities} />
-        <Paws
-          pawsParam={paws}
-          numOfRowsParam={numOfRows}
-          pageNoParam={pageNo}
-          totalCountParam={totalCount}
-        />
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <Paws />
+        </HydrationBoundary>
       </main>
     </Suspense>
   );
