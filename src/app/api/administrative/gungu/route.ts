e69d2b19d53ddced42ee2103811db75b18396c35
chulphan <chkim100617@gmail.com>
2025-05-14 15:03:27 +0900

    광역시/도, 시도군구 관련 작업 - app router 생성 - supabase 에 데이터 저장/불러오기

--- src/app/api/administrative/gungu/route.ts
+++ src/app/api/administrative/gungu/route.ts
@@ -1,16 +1,28 @@
 import { httpGet } from '@/app/_lib/util/http';
 import { FullCity, ResponseBodyType } from '@/app/_types';
+import { createClient } from '@/lib/supabase/server';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const GET = async (request: NextRequest) => {
   const searchParams = request.nextUrl.searchParams;
   const sidoCode = searchParams.get('sidoCode');
 
-  const gunguResponse = (await httpGet<FullCity>('sigungu', {
-    upr_cd: sidoCode,
-  })) as ResponseBodyType<FullCity>;
+  const supabase = await createClient();
+  const { data } = await supabase.from('gungu').select().eq('uprCd', sidoCode);
 
-  const gunguList = gunguResponse.items.item;
+  if (!data || data?.length === 0) {
+    const gunguResponse = (await httpGet<FullCity>('sigungu', {
+      upr_cd: sidoCode,
+    })) as ResponseBodyType<FullCity>;
 
-  return NextResponse.json(gunguList);
+    const gunguList = gunguResponse.items.item;
+
+    const { error } = await supabase.from('gungu').upsert(gunguList);
+
+    console.log('error ', error);
+
+    return NextResponse.json(gunguList);
+  }
+
+  return NextResponse.json(data);
 };
--- src/app/api/administrative/sido/route.ts
+++ src/app/api/administrative/sido/route.ts
@@ -1,13 +1,24 @@
 import { httpGet } from '@/app/_lib/util/http';
 import { City, ResponseBodyType } from '@/app/_types';
+import { createClient } from '@/lib/supabase/server';
 import { NextResponse } from 'next/server';
 
 export const GET = async () => {
-  const sidoResponse = (await httpGet<City>('sido', {
-    numOfRows: 17,
-  })) as ResponseBodyType<City>;
+  const supabase = await createClient();
 
-  const sidoList = sidoResponse.items.item;
+  const { data } = await supabase.from('sido').select();
 
-  return NextResponse.json(sidoList);
+  if (!data || data?.length === 0) {
+    const sidoResponse = (await httpGet<City>('sido', {
+      numOfRows: 17,
+    })) as ResponseBodyType<City>;
+
+    const sidoList = sidoResponse.items.item;
+
+    await supabase.from('sido').upsert(sidoList);
+
+    return NextResponse.json(sidoList);
+  }
+
+  return NextResponse.json(data);
 };
