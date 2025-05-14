520b89c11002a93fff11edae2fede330cb2ccc9c
chulphan <chkim100617@gmail.com>
2025-05-14 17:43:52 +0900

    유기동물 목록 조회 useQuery hook 생성

---
+++ src/app/_lib/hooks/react-query/usePaws.ts
@@ -0,0 +1,55 @@
+import { infiniteQueryOptions } from '@tanstack/react-query';
+import { getPaws } from '../../api';
+
+export type PawsQueryParamProps = {
+  pageNo: number;
+  numOfRows: number;
+  totalCount: number;
+};
+
+const pawsQueryDefaultParams = {
+  pageNo: 1,
+  numOfRows: 48,
+  totalCount: 0,
+};
+
+export const pawsQueryOptions = ({
+  pageNo,
+  numOfRows,
+  totalCount,
+}: PawsQueryParamProps = pawsQueryDefaultParams) => {
+  return infiniteQueryOptions({
+    queryKey: ['paws'],
+    queryFn: async ({ pageParam }) => {
+      const { pageNo, numOfRows, totalCount } = pageParam;
+
+      return await getPaws({
+        pageNo,
+        numOfRows,
+        totalCount,
+      });
+    },
+    initialPageParam: {
+      pageNo,
+      numOfRows,
+      totalCount,
+    },
+    getNextPageParam: (lastPage) => {
+      const totalPage =
+        (lastPage.totalCount ?? 0 % (lastPage.numOfRows ?? 48) === 0)
+          ? Math.floor((lastPage.totalCount ?? 0) / (lastPage.numOfRows ?? 48))
+          : Math.floor((lastPage.totalCount ?? 0) / (lastPage.numOfRows ?? 48)) + 1;
+
+      const hasNextPage = (lastPage.pageNo ?? 1) <= totalPage;
+      if (hasNextPage) {
+        return {
+          pageNo: (lastPage.pageNo ?? 1) + 1,
+          totalCount: lastPage.totalCount,
+          numOfRows: lastPage.numOfRows,
+        };
+      }
+
+      return undefined;
+    },
+  });
+};
