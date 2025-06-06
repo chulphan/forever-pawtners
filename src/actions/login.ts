c0b9e352545cbae03db99f05b46167afad7b65bb
chulphan <chkim100617@gmail.com>
2025-06-06 19:05:01 +0900

    로그인 redirect url 변경

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -7,10 +7,19 @@ import { redirect } from 'next/navigation';
 export async function loginWithKakao() {
   const supabase = await createClient();
 
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/';
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith('http') ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.endsWith('/') ? url : `${url}/`;
+
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'kakao',
     options: {
-      redirectTo: `${getBaseUrl()}/auth/callback`,
+      redirectTo: `${url}auth/callback`,
     },
   });
 
