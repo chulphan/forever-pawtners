d65dba4f341922599aa229b3b3d861c57b3322fd
chulphan <chkim100617@gmail.com>
2025-06-11 16:37:34 +0900

    rollback

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
-
 const nextConfig = {
   images: {
     // domains: ['www.animal.go.kr'],
@@ -24,22 +11,9 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'www.openai.animal.go.kr',
+        hostname: 'openai.animal.go.kr',
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
-          },
-        ],
-      },
-    ];
   },
 };
 
